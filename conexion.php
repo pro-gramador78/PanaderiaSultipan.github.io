f4afbddd6de581967d14879974aa6cb8f7143753
pro-gramador78 <ericksebastianrodriguezromero@gmail.com>
2025-07-17 13:46:31 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,11 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "sultipan");
+$conexion = new mysqli(
+    getenv("MYSQLHOST"),
+    getenv("MYSQLUSER"),
+    getenv("MYSQLPASSWORD"),
+    getenv("MYSQLDATABASE"),
+    getenv("MYSQLPORT")
+);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
